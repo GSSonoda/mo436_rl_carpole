479e40bb7706c0581d60d5be7b424d8f248a3125
g.sonoda <g.sonoda@sidi.org.br>
2025-12-10 11:20:54 -0300

    add continuos env

--- src/env/carpole_continuos_deterministic.py
+++ src/env/carpole_continuos_deterministic.py
@@ -1,18 +1,15 @@
 import gymnasium as gym
 import numpy as np
 
-from src.env.discretizer import Discretizer
 
-
-class CartPoleDeterministic(gym.Wrapper):
-    def __init__(self, seed: int = 10, bins=(6, 6, 12, 12)):
+class CartPoleContinuosDeterministic(gym.Wrapper):
+    def __init__(self, seed: int = 10):
         self.env = gym.make("CartPole-v1")
         super().__init__(self.env)
 
         # fix de Random Number Generation
         self.seed_value = seed
         self.np_random, _ = gym.utils.seeding.np_random(seed)
-        self.discretizer = Discretizer(bins)
 
     def reset(self, **kwargs):
         super().reset(seed=self.seed_value)
@@ -30,8 +27,8 @@ class CartPoleDeterministic(gym.Wrapper):
         )
 
         self.env.unwrapped.state = state.copy()
-        return self.discretizer.discretize(state), {}
+        return state, {}
 
     def step(self, action):
         obs, reward, done, truncated, info = self.env.step(action)
-        return self.discretizer.discretize(obs), reward, done, truncated, info
+        return obs, reward, done, truncated, info
---
+++ src/env/carpole_continuos_stochastic.py
@@ -0,0 +1,30 @@
+import gymnasium as gym
+import numpy as np
+
+
+class CartPoleContinuosStochastic(gym.Wrapper):
+    def __init__(self, force_std=0.1):
+        self.env = gym.make("CartPole-v1")
+        super().__init__(self.env)
+        self.base_env = self.env.unwrapped
+        self.force_std = force_std
+        self.np_random = np.random
+
+    def reset(self, **kwargs):
+        obs, info = super().reset(**kwargs)
+        return obs, info
+
+    def step(self, action):
+        original_force_mag = self.base_env.force_mag
+        # add noise on force
+        force = original_force_mag if int(action) == 1 else -original_force_mag
+        noise = self.np_random.normal(0, self.force_std)
+        noisy_force = force + noise
+        try:
+            self.base_env.force_mag = abs(noisy_force)
+            obs, reward, done, truncated, info = super().step(action)
+        finally:
+            # reset force_mag
+            self.base_env.force_mag = original_force_mag
+
+        return obs, reward, done, truncated, info
